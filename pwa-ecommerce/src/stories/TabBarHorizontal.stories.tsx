1944a3fb6d9fcfc88630f4d06986c82fa9f69312
Everton Sacramento <nazare.everton@gmail.com>
2022-11-21 00:33:02 -0300

    Refatorando os stories do  Tabbar

--- pwa-ecommerce/src/stories/TabBarHorizontal.stories.tsx
+++ pwa-ecommerce/src/stories/TabBarHorizontal.stories.tsx
@@ -1,27 +1,30 @@
 import { TabHoriz, ItemPropNav } from "../components/tabs/TabHoriz"
+import {ContextNavTab}  from "../Contexts/ContexNavTab"
+
+const items : ItemPropNav[] = [
+   {
+     item: "Label 1",
+   },
+   {
+     item: "Label 2",
+   },
+   {
+     item: "Label 3",
+   },
+ ]
+
+const Content : React.FC = () => {
+   return(
+      <ContextNavTab>
+         <TabHoriz array = {items}/> 
+      </ContextNavTab>
+   )
+}
+
 
 export default {
-    title: 'Components/Tab',
-    component: TabHoriz,
-    
-    args : {
-        array: [
-            {
-               item: "Home",
-    
-            },
-            {
-                item: "Handbag",
-     
-             },
-             {
-                item: "Label",
-     
-             }
-    
-          ]
-    }
-
+    title: 'Components/Tabbar',
+    component: Content,
 }
 
 export const Default = {}
